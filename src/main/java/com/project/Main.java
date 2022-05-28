c7e1a515f676dbdb238c8225e4f3847da5f5c1c1
ahmed <ahmd66699@gmail.com>
2022-05-28 22:24:49 +0300

    Setup project

---
+++ src/main/java/com/project/Main.java
@@ -0,0 +1,21 @@
+package com.project;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String pRule;
+        System.out.println("Enter prediction rules :");
+        pRule=scanner.nextLine();
+        //calculate
+
+
+
+    }
+
+
+
+
+
+}
