baf600e4ebe55a0dea47d21cc30c407738be674f
ahmed <ahmd66699@gmail.com>
2022-05-29 16:55:23 +0300

    Prodcution Rule class

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -4,49 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String pRule = "{S -> Ab|a,A - > a|`}";
-
-
-        ProductionRules[] productionRules = new ProductionRules[4];
-        productionRules[0] = new ProductionRules("S", "Ab");
-        productionRules[1] = new ProductionRules("SS", "aAA");
-        productionRules[2] = new ProductionRules("A", "ab");
-        productionRules[3] = new ProductionRules("B", "aA");
-        for (ProductionRules productionRule : productionRules) {
-            System.out.println(Grammar.type(productionRule));
-        }
-
 
     }
 }
 
-class Grammar {
-
-    public static int type(ProductionRules productionRules) {
-        //type 3
-        if (
-                productionRules.leftIsVn()
-                && (productionRules.rightSideNumber() <= 2)
-                && (productionRules.RightSideVnNumber() <= 1 && productionRules.RightSideVtNumber() <= 1)
-        ) return 3;
-        //start with ` in S
-        if (
-                productionRules.leftSideNumber() == 1
-                        && productionRules.leftIsVn()
-        )return 2;
-
-
-
-        return 0;
-    }
-    public static boolean condType1(ProductionRules productionRules){
-        String left=productionRules.getLeftSideOfProductionRules();
-        String right=productionRules.getRightSideOfProductionRules();
-
-
-        return false;
-    }
-
-
-}
--- src/main/java/com/project/ProductionRules.java
+++ src/main/java/com/project/ProductionRules.java
@@ -17,6 +17,7 @@ public class ProductionRules {
     public int rightSideNumber() {
         return rightSideOfProductionRules.length();
     }
+
     //a b c
     public int leftSideVtNumber() {
         int number = 0;
@@ -33,6 +34,7 @@ public class ProductionRules {
         }
         return number;
     }
+
     //A B S C
     public int leftSideVnNumber() {
         int number = 0;
@@ -49,15 +51,68 @@ public class ProductionRules {
         }
         return number;
     }
+
     //cheek if leftSide is A
     public boolean leftIsVn() {
         return leftSideNumber() == 1 && isVn(leftSideOfProductionRules.charAt(0));
     }
 
-    public boolean isVt(char c){
+    public boolean isVt(char c) {
         return c >= 97 && c <= 122;
     }
-    public boolean isVn(char c){
+
+    public boolean isVn(char c) {
         return c >= 65 && c <= 90;
     }
+
+    public int type() {
+
+
+        //type 3
+        if (
+                leftIsVn()
+                        && (rightSideNumber() <= 2)
+                        && (RightSideVnNumber() <= 1 && RightSideVtNumber() <= 1)
+        ) return 3;
+        //start with ` in S
+        if (
+                leftSideNumber() == 1
+                        && leftIsVn()
+        ) return 2;
+
+        if (
+                condType1()
+                        &&leftSideNumber()<=rightSideNumber()
+
+        ) return 1;
+
+        return 0;
+    }
+
+    public boolean condType1() {
+        String left = getLeftSideOfProductionRules();
+        String right = getRightSideOfProductionRules();
+        //Alpha one and two
+        String $1l, $2l;
+        String $1r, $2r;
+
+
+        for (int i = 0; i < left.length(); i++) {
+
+            if (isVn(left.charAt(i))) {
+                $1l = left.substring(0, i);
+                $2l = left.substring(i + 1);
+                $1r = right.substring(0, i);
+                $2r = right.substring(right.length() - $2l.length());
+                //System.out.println("Alpha one : "+$1l+" == "+$1r+"\t Alpha two :  "+$2l+" == "+$2r);
+                if (!($1l.length() + $2l.length() == right.length()))
+                    if ($1l.equals($1r) && $2l.equals($2r)) return true;
+
+            }
+
+        }
+        return false;
+    }
+
+
 }
